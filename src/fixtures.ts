6cc87b3fda441f483e29d3cebfc4d486f98a6228
michael120288 <78063023+michael120288@users.noreply.github.com>
2026-06-08 21:16:35 -0400

    feature/signup-add to fixtures values for signup, created test for signup user (#2)
    * add to fixtures values for signup, created test for signup user
    
    * created test for signup user

--- src/fixtures.ts
+++ src/fixtures.ts
@@ -1,1 +1,5 @@
 export const TEST_CLEANUP_SECRET = 'chatty-test-cleanup-2026';
+export const TEST_AVATAR_IMAGE =
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAAEklEQVR4nGPwmvAID2IYlcaGAM6+rXFsLAOFAAAAAElFTkSuQmCC';
+
+export const TEST_AVATAR_COLOR = '#4a90e2';
